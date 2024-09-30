314122e001c0849999a29a8e2310410c7cf80f5c
Yash Mimani <yashmimani3107@gmail.com>
2024-09-30 20:09:23 +0530

    about jsx , babel , react functional component

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
-//nested components
-/**
- *
- * <div id =parent>
- * <div id:"child">
- * <h1> "i am h1 tag</h1>
- * </div>
- *
- * </div>
- *
- *
- *
- */
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]
-  )
+
+// React Element
+// recat.craeteelement -> react lement -> js object -> rendered
+const heading = React.createElement(
+  "h1",
+  {id:"heading"},
+  "Namaste Javascript"
 );
+console.log(heading);
 
-// const heading = React.createElement(
-//   "h1",
-//   {
-//     id: "heading",
-//     xyz: "abc",
-//   },
-//   "Hello World From React"
-// );
+// JSX -> craeting javascript syntax(HTML like syntax)
+// JSX -> babel transpiles it-> react.createElement -> js object -> HTml object rendered
+//if it is component then start with capital letter
+const JsxHeading = () => (
+  <h1 id="heading1">Namaste Reacting using JSX</h1>
+);
+// console.log(jsxHeading);
 
-console.log(parent); // javascript object or react element
+// React Component
+// Class based component -> old way
+// functional component -> new 
+
+// React Functional Component
+
+const HeadingComponent = () => (
+  <div id="container"> 
+     <JsxHeading />
+     <h1>Namaste Javascript from react Functional Component</h1>
+     </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent/> );
+
